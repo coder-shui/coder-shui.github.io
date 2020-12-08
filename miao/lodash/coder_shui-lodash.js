c90152af14c18557ff748e6d76621359ce1c264b
waterLittleRain <811983255@qq.com>
2020-12-08 16:03:03 +0800

    lodash2

--- miao/lodash/coder_shui-lodash.js
+++ miao/lodash/coder_shui-lodash.js
@@ -1,4 +1,4 @@
-let coder_shui = function () {
+var coder_shui = function () {
   function chunk(array,size) {
     if (array.length < size) {
       return array
@@ -18,10 +18,29 @@ let coder_shui = function () {
     }
     return arr
   }
-
-
+  function compact(array) {
+    let res = []
+    for (let i = 0; i < array.length; i++) {
+      if(array[i]) res.push(array[i])
+    }
+    return res
+  }
+  function concat() {
+    if (!arguments[0]) return undefined
+    let arr = arguments[0]
+    for (let i = 1; i < arguments.length; i++) {
+      if (Array.isArray(arguments[i])) {
+        for (let j = 0; j < arguments[i].length; j++) {
+          arr.push(arguments[i][j])
+        }
+      }else if(arguments[i] !== undefined && arguments[i] !== null && arguments[i] == arguments[i]) arr.push(arguments[i])
+    }
+    return arr
+  }
 
   return {
+    concat,
+    compact,
     chunk
 }
 }()
