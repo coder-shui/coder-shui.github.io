db1cf3b427bbbd75c1dfeeb05cfe01fab24fcba2
waterLittleRain <811983255@qq.com>
2020-12-08 15:26:37 +0800

    lodash1

--- miao/lodash/coder_shui-lodash.js
+++ miao/lodash/coder_shui-lodash.js
@@ -1,11 +1,27 @@
 let coder_shui = function () {
-  function chunk() {
-    
+  function chunk(array,size) {
+    if (array.length < size) {
+      return array
+    }
+    let temp
+    let arr = []
+    let i = 0
+    while (i < array.length){
+      temp = size
+      let t = []
+      while (temp) {
+        if(array[i])t.push(array[i])
+        i++
+        temp--
+      }
+      arr.push(t)
+    }
+    return arr
   }
 
 
 
   return {
-  
+    chunk
 }
 }()
